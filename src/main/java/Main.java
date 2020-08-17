f6830165470115b01f723a92b34f603248c59982
Michael30502 <54103011+Michael30502@users.noreply.github.com>
2020-08-17 10:58:04 +0200

    Opgave 1 - 8,5, not running

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,61 +1,79 @@
 import processing.core.PApplet;
 
-public class Main {
-    import processing.core.PApplet;
+public class Main extends PApplet{
+    //....Denne variabel kan bruges til at
+    static PApplet p;
 
-    public class Main extends PApplet{
-        //....Denne variabel kan bruges til at
-        static PApplet p;
+    //Denne liste SKAL indholde alle de "Ting", der er pÃ¥ skÃ¦rmen!
+    Ting[] listeTing;
 
-        //Denne liste SKAL indholde alle de "Ting", der er pÃ¥ skÃ¦rmen!
-        Ting[] listeTing;
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
 
-        public static void main(String[] args) {
-            PApplet.main("Main");
+    @Override
+    public void settings() {
+        super.settings();
+        size(500, 500);
+
+        //...Alle processing kommandoer er nu inde i p....
+        p = this;
+
+
+        //Opgave 1 : Her skal du oprette listen "listeTing" med plads til 20 ting
+
+        listeTing = new Ting[20];
+
+        //Opgave 5 : Her skal du skrive kode der laver alle "Ting"
+        for(int i =0;i< listeTing.length;i++){
+
+listeTing[i] = new Ting(random(21,width-21),random(21,height-21),this,listeTing);
+        }
+    }
+
+
+    public void draw(){
+        //... Eksempel pÃ¥ processing kommando (som den bÃ¸r bruges i andre klasser!!)
+        background(200,100,100);
+        Main.p.fill(200,200,200);
+        Main.p.rect(10,10,480,480);
+
+        //OPGAVE 8 (SVÃ†R) : Her skal du skrive kode der Ã¦ndrer farven til rÃ¸d din "Ting"  rÃ¸rer ved musen...
+        for(int i =0;i< listeTing.length;i++){
+
+            if(listeTing[i].becomeBlue())
+    fill(0,0,255);
+            else
+                fill(255,255,255);
+
+
+        //Opgave 6 : Her skal du skrive kode, der tegner alle "Ting"
+        //ps: Du SKAL kalde "tegn metoden" pÃ¥ ALLE "Ting"
+
+
+
+            listeTing[i].tegn();
+
         }
 
-        @Override
-        public void settings() {
-            super.settings();
-            size(500,500);
 
-            //...Alle processing kommandoer er nu inde i p....
-            p = this;
+        //Opgave 7 : Her skal du skrive kode, der fÃ¥r alle "Ting" til at flytte sig
+        //ps: DU SKAL kalde "
+        for(int i =0;i< listeTing.length;i++){
 
-
-            //Opgave 1 : Her skal du oprette listen "listeTing" med plads til 20 ting
-
-            //Opgave 5 : Her skal du skrive kode der laver alle "Ting"
-
-
-        }
-
-        public void draw(){
-            //... Eksempel pÃ¥ processing kommando (som den bÃ¸r bruges i andre klasser!!)
-            Main.p.fill(200,200,200);
-            Main.p.rect(10,10,480,480);
-
-            //OPGAVE 8 (SVÃ†R) : Her skal du skrive kode der Ã¦ndrer farven til rÃ¸d din "Ting"  rÃ¸rer ved musen...
-
-
-            //Opgave 6 : Her skal du skrive kode, der tegner alle "Ting"
-            //ps: Du SKAL kalde "tegn metoden" pÃ¥ ALLE "Ting"
-
-
-
-            //Opgave 7 : Her skal du skrive kode, der fÃ¥r alle "Ting" til at flytte sig
-            //ps: DU SKAL kalde "
+        listeTing[i].flyv();
+    }
 
 
 
 
 
-            //EKSPERT 2:
-            //Hvis to ting rÃ¸rer hinanden skal de blive blÃ¥!
 
-        }
-
-
+        //EKSPERT 2:
+        //Hvis to ting rÃ¸rer hinanden skal de blive blÃ¥!
 
     }
+
+
+
 }
--- src/main/java/Ting.java
+++ src/main/java/Ting.java
@@ -1,4 +1,4 @@
-public class Ting {
+
     import processing.core.PApplet;
 
     public class Ting {
@@ -7,20 +7,56 @@ public class Ting {
         //Tings position
         float x, y;
         float colorR = 100;
+        PApplet p;
+        Ting[] listeTing;
 
-        Ting(float inputX, float inputY){
+        Ting(float inputX, float inputY,PApplet p,Ting[] listeTing){
 
             //Opgave 2: Her skal du skrive kode der sÃ¦tter positionen for tingen x og y
-
+this.listeTing = listeTing;
+x= inputX;
+y= inputY;
+this.p = p;
         }
 
         void tegn(){
             //Opgave 3: Her skal du skrive kode der tegner denne "Ting"
+p.ellipse(x,y,20,20);
+        }
+
+        boolean becomeBlue(){
+            boolean result= false;
+            if((p.mouseX< x+20 && p.mouseX> x-20)&&(p.mouseY >y-20 && p.mouseY< y+20))
+                result = true;
+
+            for(int i =0;i< listeTing.length;i++){
+if(listeTing[i].x < x){
+
+}
+
+
+            }
+
+
+                return result;
 
         }
 
         void flyv(){
             //Opgave 4: Her skal du skrive kode der flytter "Ting"
+            x += p.random(-5,5);
+            if(x<20)
+                x= 20;
+            if(x>p.width-20)
+                x=p.width-20;
+
+            y+= p.random(-5,5);
+            if(y<20)
+                y= 20;
+            if(x>p.height-20)
+             y=p.height-20;
+
+
             //ps: Husk de mÃ¥ ikke flytte sig uden for skÃ¦rmen...
 
 
@@ -30,4 +66,4 @@ public class Ting {
 
 
     }
-}
+
