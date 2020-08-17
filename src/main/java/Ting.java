cd9252b70fb438afda5edb500d6b787cc6fa367a
Michael30502 <54103011+Michael30502@users.noreply.github.com>
2020-08-17 09:45:10 +0200

    Initial commit

---
+++ src/main/java/Ting.java
@@ -0,0 +1,33 @@
+public class Ting {
+    import processing.core.PApplet;
+
+    public class Ting {
+
+
+        //Tings position
+        float x, y;
+        float colorR = 100;
+
+        Ting(float inputX, float inputY){
+
+            //Opgave 2: Her skal du skrive kode der sÃ¦tter positionen for tingen x og y
+
+        }
+
+        void tegn(){
+            //Opgave 3: Her skal du skrive kode der tegner denne "Ting"
+
+        }
+
+        void flyv(){
+            //Opgave 4: Her skal du skrive kode der flytter "Ting"
+            //ps: Husk de mÃ¥ ikke flytte sig uden for skÃ¦rmen...
+
+
+        }
+
+
+
+
+    }
+}
